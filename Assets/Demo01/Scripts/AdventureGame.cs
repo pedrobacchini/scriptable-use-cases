aa68b1fc166ec85bcb860c9269ecb492f17fc81c
Pedro Bacchini <pedrobacchini@outlook.com>
2019-11-30 08:43:18 -0300

    Scriptable Adventure Game.

---
+++ Assets/Demo01/Scripts/AdventureGame.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] private Text textComponent = null;
+    [SerializeField] private State startingState = null;
+
+    private State _state;
+
+    private void Start()
+    {
+        _state = startingState;
+        textComponent.text = _state.GetStoryText();
+    }
+
+    private void Update()
+    {
+        var nextStates = _state.GetNextStates();
+        for (var indexState = 0; indexState < nextStates.Length; indexState++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + indexState))
+            {
+                _state = nextStates[indexState];
+            }
+        }
+        textComponent.text = _state.GetStoryText();
+    }
+}
